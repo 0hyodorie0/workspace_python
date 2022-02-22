ddb8f9d2821c95a042bb468dd4e12dd8e48b06eb
Sohn Hyosun <48785364+0hyodorie0@users.noreply.github.com>
2022-02-22 17:11:11 +0900

    egov

--- HELLO_PYTHON/day03/ooptest03.py
+++ HELLO_PYTHON/day03/ooptest03.py
@@ -1,7 +1,25 @@
-class SohnHS:
+class LeeJY:
     def __init__(self):
-        self.skillSoccer = 0
+        self.money = 1000000000
         
+    def dirtymoney(self, amount):
+        self.money += amount;
+        
+class Messi:
+    def __init__(self):
+        self.levelSoccer = 100
     def getOlder(self):
-        self.skillSoccer += 1
-    
+        self.levelSoccer -= 1
+        
+class SohnHS(LeeJY, Messi):
+    def __init__(self):
+        LeeJY.__init__(self)
+        Messi.__init__(self)
+        
+if __name__ == '__main__':
+    a = SohnHS()
+    print(a.money)
+    print(a.levelSoccer)
+    a.dirtymoney(10000000000000000)
+    a.getOlder()
+    print(a.money)
