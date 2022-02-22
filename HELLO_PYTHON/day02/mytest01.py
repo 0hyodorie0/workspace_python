66b6c1d1235ec5822203a18e4577d1de84d5a99d
Sohn Hyosun <48785364+0hyodorie0@users.noreply.github.com>
2022-02-22 09:04:26 +0900

    homework

--- HELLO_PYTHON/day02/mytest01.py
+++ HELLO_PYTHON/day02/mytest01.py
@@ -13,8 +13,8 @@ else :
     res = "보"
     
 if (res == "가위" and i == "가위") or (res == "바위" and i == "바위") or (res == "보" and i == "보") :
-    print("컴퓨터:",res," / 나: ", i, " / 비겼습니다.")
+    print("컴퓨터:",res," / 나:", i, " // 비겼습니다.")
 elif (res == "가위" and i == "바위") or (res == "바위" and i == "보") or (res == "보" and i == "가위") :
-    print("컴퓨터:",res," / 나: ", i, " / 이겼습니다.")
+    print("컴퓨터:",res," / 나:", i, " // 이겼습니다.")
 else :
-    print("컴퓨터:",res," / 나: ", i, " / 졌습니다.")
+    print("컴퓨터:",res," / 나:", i, " // 졌습니다.")
