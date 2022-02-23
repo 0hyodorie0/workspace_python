d80ba52b2a81d1f7e55a5a3cf0b33d42ab84f101
Sohn Hyosun <48785364+0hyodorie0@users.noreply.github.com>
2022-02-23 16:57:18 +0900

    구구단 fxml

--- HELLO_FX/src/application/Main4.java
+++ HELLO_FX/src/application/Main4.java
@@ -11,64 +11,58 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 
-public class Main3 extends Application {
-	TextField txt_mine;
-    TextField txt_com;
-    TextField txt_result;
+public class Main4 extends Application {
+	
+	TextField tf;
+	TextArea ta;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-            Parent root = FXMLLoader.load(getClass().getResource("main3.fxml"));
-            Scene scene = new Scene(root,400,400);
+			
+            Parent root = FXMLLoader.load(getClass().getResource("main4.fxml"));
+            Scene scene = new Scene(root,300,300);
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setTitle("AppMain");
             primaryStage.setScene(scene);
             primaryStage.show();
             
+            tf = (TextField)scene.lookup("#tf");
+            ta = (TextArea)scene.lookup("#ta");
             Button btn = (Button)scene.lookup("#btn");
-            TextField txt_mine = (TextField)scene.lookup("#txt_mine");
-            TextField txt_com = (TextField)scene.lookup("#txt_com");
-            TextField txt_result = (TextField)scene.lookup("#txt_result");
-            
             btn.setOnMouseClicked(new EventHandler<Event>() {
 
 				@Override
 				public void handle(Event event) {
 					myclick();
+					
 				}
+            		
             });
             
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void myclick() {
-		String mine = txt_mine.getText();
-		String com = "";
+		String mytext = tf.getText();
+		int mynum = Integer.parseInt(mytext);
 		String result = "";
 		
-		double rnd = Math.random();
-		if(rnd>0.5) {
-			com = "홀";
-		}else {
-			com = "짝";
-		}
-		if(mine.equals(com)) {
-			result = "이김";
-		}else {
-			result = "짐";
+		for(int i = mynum; i<=mynum; i++) {
+			for(int j = 1; j<=9; j++) {
+				result += i+"*"+j+"="+i*j+"\n";
+			}
 		}
 		
-		txt_com.setText(com);
-		txt_result.setText(result);
-		
+		ta.setText(result);
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
