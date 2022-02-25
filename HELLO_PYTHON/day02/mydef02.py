ef98c7717fa4a6ee9beb9102fea05583711fda78
Sohn Hyosun <48785364+0hyodorie0@users.noreply.github.com>
2022-02-25 14:33:11 +0900

    gugudan

--- HELLO_PYTHON/day02/mydef02.py
+++ HELLO_PYTHON/day02/mydef02.py
@@ -1,4 +1,17 @@
+# def showDan(dan):
+#     for i in range(1,9+1):
+#         res = dan * i 
+#         print(dan,"*",i,"=",res)
+
 def showDan(dan):
-    for i in range(1,9+1):
-        res = dan * i
-    print(dan, "*", i, "=", res)
+    print(dan,"*",1,"=",(dan*1))
+    print(dan,"*",2,"=",(dan*2))
+    print(dan,"*",3,"=",(dan*3))
+    print(dan,"*",4,"=",(dan*4))
+    print(dan,"*",5,"=",(dan*5))
+    print(dan,"*",6,"=",(dan*6))
+    print(dan,"*",7,"=",(dan*7))
+    print(dan,"*",8,"=",(dan*8))
+    print(dan,"*",9,"=",(dan*9))
+
+showDan(3)
